ec9989f38a893eaafde2eb4d3e1bb29e267b5e1d
Ana Medrano <amedrano.fernandez@gmail.com>
2020-04-17 16:53:43 -0700

    view department, create department functions

--- app.js
+++ app.js
@@ -13,4 +13,21 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
     if(err) throw err;
     console.log ("Connected as ID " + connection.threadId + "\n");
+    addDepartment(10,"Legal")
+    viewDepartments()
 });
+
+
+function viewDepartments(){
+    connection.query("SELECT * FROM department", function(err,res){
+        if(err) throw err;
+        console.log(res)
+    })
+}
+
+function addDepartment(id,newDept){
+    connection.query(`INSERT INTO department(id,name) VALUES (${id},"${newDept}")`, function (err){
+        if(err) throw err;
+        console.log("Success!")
+    })
+}
