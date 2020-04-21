48e67cafaddc5b53e3b0093e4a8127136bf84aa6
Ana Medrano <amedrano.fernandez@gmail.com>
2020-04-20 17:18:14 -0700

    updated read-me and added seed data

--- README.md
+++ README.md
@@ -1,1 +1,81 @@
-# employee_tracker
+# Employee Tracker READ ME
+        
+## Summary
+        
+This is a command line interface that allows members of the HR team to add and view departments, roles and employee information. The data is stored in SQL.
+
+<img src='https://img.shields.io/static/v1?label=License&message=MIT&color=brightgreen'> 
+
+## Site Picture
+
+![site](/assets/site_image.gif)
+
+## Table of Contents
+
+- [Technologies Used](#Technologies-Used)
+- [Learning Points](#Learning-Points)
+- [Use](#Use)
+- [Installation](#Installation)
+- [Code Snippets](#Code-Snippets)
+- [Usage](#Usage)
+- [Contributing](#Contributing)
+- [Author](#Author)
+        
+## Technologies Used
+- mysql - used to store and retrieve data
+- Javascript / Node - used to build an interactive command line interfase tool
+- HTML and Bootstrap - used in provided code
+- Git - version control system for tracking changes to code
+- Github - to host the repository
+- Heroku - to host the deployed site
+
+## Learning Points
+- It is fairly simple to create a CLI that retrieves data from a database.
+- Complexity comes from how user friendly the application is and with properly timing information from the user and the sql queries.
+
+## Dependencies
+```
+mysql
+inquirer
+console.table
+```
+
+## Installation
+- You will need to download this folder into your computer either by cloning the git repository or downloading the files are a zip.
+- Once the files are accessible in tour local computer, navigate inside the employee tracker folder and do an install of the following packages by using: 
+```
+npm install mysql
+npm install inquirer
+npm install console.table
+```
+- Once you have successfully installed those packages, you can run the program by typing the following command to your terminal
+```
+node app.js
+```
+        
+        
+## Usage
+- There is seed data for department, role and employee on the assets folder that can be used to run view functionality.
+- In order to create employees, the relevant role and department data needs to exist in the role and department tables.
+- The recommended order for adding data is to start with departments, then role then employees.
+
+## Code Snippet
+The code below was used in the application a few times and it highlights my increased comfort with map functions. It also shows the use of console.table which allows json data to be parsed into a nice looking table without much effort.
+
+```
+       var parsedRes = cTable.getTable(res.map(element => { return { id: element.employeeID, name: element.first_name + " " + element.last_name, title: element.title, salary: element.salary, manager: element.manager_ID ,department: element.department } }));
+        console.log(parsedRes)
+        ```
+        
+         
+## Contributors
+            
+None
+         
+        
+## Author
+[GitHub](https://github.com/analoo)
+
+<img src='https://avatars3.githubusercontent.com/u/8609011?v=4' alt = "my-avatar" style = "width: 40px; border-radius: 15px;"/>
+       
+        
--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var mysql = require("mysql")
 var inquirer = require("inquirer")
 var cTable = require('console.table');
 
-
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -13,40 +12,47 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("Connected as ID " + connection.threadId + "\n");
-    inquirer.prompt({
+    promptUser();
+
+
+});
+
+function promptUser(){
+    return inquirer.prompt({
         type: "list",
         message: "What would you like to do?",
         name: "option",
-        choices: ["View Roles", "View Employees", "View Departments", "Add Roles", "Add Employees", "Add Departments"]
-        
-    }).then(function(res){
-        switch(res.option){
-            case("View Roles"):
+        choices: ["View Roles", "View Employees", "View Departments", "Add Roles", "Add Employees", "Add Departments", "Update Employee Role"]
+
+    }).then(function (res) {
+        switch (res.option) {
+            case ("View Roles"):
                 viewRoles();
                 break;
-            case("View Employees"):
+            case ("View Employees"):
                 viewEmployees();
                 break;
-            case("View Departments"):
+            case ("View Departments"):
                 viewDepartments();
                 break;
-            case("Add Roles"):
+            case ("Add Roles"):
                 addRoles();
                 break;
-            case("Add Employees"):
+            case ("Add Employees"):
                 addEmployees();
                 break;
-            case("Add Departments"):
+            case ("Add Departments"):
                 addDepartment();
+                break;
+            case ("Update Employee Role"):
+                updateRole();
                 break;
             default:
                 console.log("You picked default")
         }
-        
-    })
 
-});
+    })
+}
 
 
 
@@ -56,15 +62,24 @@ function viewDepartments() {
         if (err) throw err;
         var parsedRes = cTable.getTable(res.map(element => { return { id: element.id, department: element.name } }));
         console.log(parsedRes)
+        promptUser()
     })
 }
 
 
-function addDepartment(id, newDept) {
-    connection.query(`INSERT INTO department(id,name) VALUES (${id},"${newDept}")`, function (err) {
-        if (err) throw err;
-        console.log("Success!")
-        viewDepartments()
+function addDepartment() {
+    inquirer.prompt({
+        type: "input",
+        name: "dept_name",
+        message: "What is the name of this new dept?"
+    })
+    .then(function (data) {
+        connection.query(`INSERT INTO department (name) VALUES ("${data.dept_name}")`, function (err) {
+            if (err) throw err;
+            console.log("Successfully added this new department!")
+            viewDepartments()
+        });
+
     });
 };
 
@@ -74,15 +89,33 @@ function viewRoles() {
         if (err) throw err;
         var parsedRes = cTable.getTable(res.map(element => { return { id: element.role_id, title: element.title, salary: element.salary, department: element.name } }));
         console.log(parsedRes)
+        promptUser()
+
     })
 }
 
-function addRoles(id, title, salary, department){
-    connection.query(`INSERT INTO role(id,title,salary, department_id) VALUES (${id}, "${title}", ${salary}, ${department})`, function (err){
+function addRoles() {
+    inquirer.prompt([{
+        type: "input",
+        name: "title",
+        message: "What is the title of the role?"
+    }, {
+        type: "input",
+        name: "salary",
+        message: "What is the annualized salary?"
+    }, {
+        type: "input",
+        name: "dept_id",
+        message: "What department id does this role belong to?"
+    }])
+    .then(function (data) {
+    connection.query(`INSERT INTO role(title,salary, department_id) VALUES ("${data.title}", ${data.salary}, ${data.dept_id})`, function (err) {
         console.log("Success! - You added a new role")
-        viewRoles() 
-    })
-}
+        viewRoles()
+    });
+
+});
+};
 
 
 function viewEmployees() {
@@ -91,23 +124,45 @@ function viewEmployees() {
     LEFT JOIN role on employee.role_ID = role.id
     LEFT JOIN department on role.department_ID = department.id`, function (err, res) {
         if (err) throw err;
-        var parsedRes = cTable.getTable(res.map(element => { return { id: element.employeeID, name: element.first_name +" "+element.last_name, title: element.title, salary: element.salary, department: element.department } }));
+        var parsedRes = cTable.getTable(res.map(element => { return { id: element.employeeID, name: element.first_name + " " + element.last_name, title: element.title, salary: element.salary, manager: element.manager_ID ,department: element.department } }));
         console.log(parsedRes)
-    
+        promptUser()
+
     })
 }
 
 
-function addEmployees(id, first,last,role_id,mgr_id){
-    connection.query(`INSERT INTO employee(id,first_name,last_name, role_id, manager_id) VALUES (${id}, "${first}", "${last}",${role_id}, ${mgr_id})`, function (err){
-        if(err) throw err;
+function addEmployees() {
+    inquirer.prompt([{
+        type: "input",
+        name: "first",
+        message: "First Name: "
+    }, {
+        type: "input",
+        name: "last",
+        message: "Last Name: "
+    }, {
+        type: "input",
+        name: "role_id",
+        message: "Role id:"
+    },
+    {
+        type: "input",
+        name: "mgr_id",
+        message: "Manager id:"
+    }])
+    .then(function (data) {
+    connection.query(`INSERT INTO employee(first_name,last_name, role_id, manager_id) VALUES ("${data.first}", "${data.last}",${data.role_id}, ${data.mgr_id})`, function (err) {
+        if (err) throw err;
         console.log("Success! - You added a new employee")
         viewEmployees()
-    })
-}
+    });
+
+});
+};
 
 
-function updateRole(ee_id,role_id){
+function updateRole(ee_id, role_id) {
     connection.query(`UPDATE employee SET role_ID = ${role_id} WHERE id=${ee_id}`)
     viewEmployees()
 }
